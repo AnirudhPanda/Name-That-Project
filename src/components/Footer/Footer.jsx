4af80d2f7e8d871d5104fba263382a4d12b0addf
Anirudh Panda <anirudhpandaofficial@gmail.com>
2021-05-14 01:03:31 +0530

    initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <h4>Made By <a className="footer-text" href="https://anirudhpanda.me/" target="_blank" rel="noopener noreferrer">Anirudh Panda</a></h4>
+        </div>
+    )
+}
+
+export default Footer
